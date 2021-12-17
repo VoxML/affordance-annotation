8ce7e0abc9611ee481ebb10d360775b5a2e0448c
henlein <henlein@em.uni-frankfurt.de>
2021-12-17 22:34:15 +0100

    Model Visualization Tool

--- HicoDetTool/utils/utils.py
+++ HicoDetTool/utils/utils.py
@@ -48,6 +48,60 @@ def get_iou(bb1, bb2):
     assert iou <= 1.0
     return iou
 
+def merge_bboxes(annotation, threshold=0.5):
+    """
+    Merges BBoxes between multiple Hois
+    :param annotation: json
+    :param threshold: float
+    :return: json; Merged Annotation with global bboxes
+    """
+    human_bboxes = []  # List with merged human_bboxes.
+    object_bboxes = []  # List with merged object_bboxes.
+
+    for hois in annotation["hois"]:
+        for h_idx, h_bbox in enumerate(hois["human_bboxes"]):
+            found = False  # To check if there was a mergable bbox found.
+            for ref_idx, ref_box in enumerate(human_bboxes):
+                iou = get_iou({"x1": h_bbox[0], "x2": h_bbox[2], "y1": h_bbox[1], "y2": h_bbox[3]}, {"x1": ref_box[0], "x2": ref_box[2], "y1": ref_box[1], "y2": ref_box[3]})
+                if iou > threshold:  # If intersection_over_union is over Threshold, Merge
+                    for connection in hois["connections"]:
+                        if connection[0] == h_idx:  # Change connections to global ids
+                            connection[0] = ref_idx
+                            found = True
+                            break
+            if not found:  # If no fitting bbox was found in the global list. Add this one.
+                human_bboxes.append(h_bbox)
+                for connection in hois["connections"]:  # Change connections to global ids
+                    if connection[0] == h_idx:
+                        connection[0] = len(human_bboxes) - 1
+
+        # Do the same with the object_bboxes
+        for b_idx, b_bbox in enumerate(hois["object_bboxes"]):
+            found = False
+            for ref_idx, ref_box in enumerate(object_bboxes):
+                iou = get_iou({"x1": b_bbox[0], "x2": b_bbox[2], "y1": b_bbox[1], "y2": b_bbox[3]}, {"x1": ref_box[0], "x2": ref_box[2], "y1": ref_box[1], "y2": ref_box[3]})
+                if iou > threshold:
+                    for connection in hois["connections"]:
+                        if connection[1] == b_idx:
+                            connection[1] = ref_idx
+                            found = True
+                            break
+            if not found:
+                object_bboxes.append(b_bbox)
+                for connection in hois["connections"]:
+                    if connection[1] == b_idx:
+                        connection[1] = len(object_bboxes) - 1
+
+        # Delete the old bboxes. connection ids now refere to the global bboxes.
+        hois.pop("human_bboxes")
+        hois.pop("object_bboxes")
+
+    # Add the global bboxes to the annotation
+    annotation["human_bboxes"] = human_bboxes
+    annotation["object_bboxes"] = object_bboxes
+
+    return annotation
+
 
 def load_hoi_annotation(path):
     hoi_annotation = {}  # {obj#verb: T/G/-}
